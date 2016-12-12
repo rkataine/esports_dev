f8dbca2c60c18005a77e25b402f9e97ff9a97664
rkataine <riku.katainen@helsinki.fi>
2016-12-12 21:29:37 +0200

    New initiation

--- scripts/filereader.js
+++ scripts/filereader.js
@@ -1,10 +1,30 @@
-function readFile(file, out) {
+function readFile(file, out, out2) {
+	
 var http = new XMLHttpRequest();
 http.open('get', file);
-http.onreadystatechange = function () {
-	out.innerHTML = http.responseText;
+http.onreadystatechange = function () {	
+	
+	if (http.readyState == 4 && http.status == 200) {
+        if (http.responseText) {
+            out.innerHTML = http.responseText;			
+			var x = out.getElementsByTagName("h2");
+			
+			var i;
+			var result = "<input id='0' value=\"#top\" onclick=\"gotoHeader()\" type='radio' name='sub_radios' checked autocomplete='off'/><label for='0'><img src='images/napit_MAIN/empty.png' class='content_button'/></label><br>";
+			
+			for (i = 0; i < x.length; i++) {
+				var rect = x[i].getBoundingClientRect();
+				
+				result += "<input id='"+x[i].id +"' value=\""+parseInt(rect.top,10) +"\" onclick=\"gotoHeader()\" type='radio'  name='sub_radios'/><label for='"+x[i].id +"'><img src='images/napit_MAIN/empty.png' class='content_button'/></label><br>\n";
+				
+			}
+					
+				
+			out2.innerHTML = result;
+			
+			
+        }
+    }	
 };
 http.send();
 }
-
-readFile('database/description.txt', document.getElementById('text-file'));
