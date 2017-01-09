b89fc8d5ca0a9af51c587e72e0926818323adb32
rkataine <riku.katainen@helsinki.fi>
2017-01-10 01:02:55 +0200

    Uusiksi koodattu menusysteemi

--- scripts/filereader.js
+++ scripts/filereader.js
@@ -1,4 +1,5 @@
-function readFile(file, out, out2, scrollpos) {
+
+function readFile(file, out, out2) {
 	
 var http = new XMLHttpRequest();
 http.open('get', file);
@@ -6,35 +7,157 @@ http.onreadystatechange = function () {
 	
 	if (http.readyState == 4) {
 		
-        if (http.responseText) {
-			
-            out.innerHTML = http.responseText;			
-			
-			var x = out.getElementsByTagName("h2");
-			
-			var i;
+        if (http.responseText) {			
+			out.innerHTML = http.responseText;			
 			var buttonclass = "class=\"main_button_left\"";
-			var result = "<input id='0' value=\"#top\" onclick=\"gotoHeader()\" type='radio' name='sub_radios' checked autocomplete='off'/><label for='0'><img src='images/espot_ikonit_empty.png' class='content_button_left'/></label><br><br>";
-			scrollpos = out.scrollTop;
-			for (i = 0; i < x.length; i++) {
-				var rect = x[i].getBoundingClientRect();
+			
+			var result = "<img name='#top' src='images/espot_ikonit_empty.png' class='content_button active'/><br><br>";
+			
+			var contentheaders = out.getElementsByTagName("h2");	
+			var buttonoffset = images[activebutton].offsetTop - images[activebutton].parentNode.getBoundingClientRect().top +18;
+			
+			for (i = 0; i < contentheaders.length; i++) {								
+				result +=  "<img name=\""+contentheaders[i].id  +"\" src='images/espot_ikonit_empty.png' class='content_button'/><br><br>\n";				
+			}
+			
+			
+			
+			out.scrollTop = 0;
+			out2.innerHTML = result;	
+			var contentbuttons = out2.getElementsByClassName("content_button");
+			
+			contentbuttons[0].style.top = buttonoffset +"px";
+			contentbuttons[0].style.left = "0px";
+			for(var i=1; i<contentbuttons.length; i++) {
 				
-				if(i%2 == 0) {
-					buttonclass = "class=\"content_button_right\"";
+				contentbuttons[i].style.top = parseInt(contentbuttons[i-1].style.top) + 23 +"px";
+				
+				if(parseInt(contentbuttons[i-1].style.left,10) < 5) {
+					contentbuttons[i].style.left = 15 +"px";
 				}
 				else {
-					buttonclass = "class=\"content_button_left\"";
+					contentbuttons[i].style.left = "0px";
 				}
-				result += "<input id='"+x[i].id +"' value=\""+parseInt(rect.top+scrollpos,10) +"\" onclick=\"gotoHeader()\" type='radio'  name='sub_radios'/><label for='"+x[i].id +"'><img src='images/espot_ikonit_empty.png' "+buttonclass +"/></label><br><br>\n";
-				
-			}
-				
-			out.scrollTop = 0;
-			out2.innerHTML = result;
-			
-			
+			}			
         }
     }	
 };
 http.send();
 }
+function makeMainClick(e) {						
+	if(e.target.nodeName == "IMG") {
+		
+		var current = e.target;
+		for(i=0; i<current.parentNode.children.length; i++) {
+			current.parentNode.children[i].classList.remove("active");
+		}
+		current.classList.add("active");
+		setMainButtons();
+		readFile('database/' +current.name, document.getElementById('text-file'), document.getElementById('content_buttons'), document.getElementById('text-file').scrollTop);					
+		
+	
+	}		
+}
+function makeContentClick(e) {						
+	if(e.target.nodeName == "IMG") {
+		var current = e.target;
+		/*
+		for(i=0; i<current.parentNode.children.length; i++) {
+			e.target.parentNode.children[i].classList.remove("active");
+		}
+		current.classList.add("active");
+		*/
+		if(current.name === "#top") {
+			 $('.content_text').animate({scrollTop:0}, 'slow');
+		}
+		else {
+			var value = current.name;			
+			var header = document.getElementById(value);			
+			$('.content_text').animate({scrollTop:header.offsetTop}, 'slow');
+		}
+	}		
+}
+
+function setMainButtons() {
+						
+						
+	for(i=0;i<images.length; i++) {
+		if(images[i].classList.contains("active")) {
+			activebutton = i;
+			break;
+		}
+	}
+	images[activebutton].style.left = "50px";
+	for(i = activebutton-1; i>= 0; i--) {
+		offset = parseInt(images[i+1].style.top,10) -40;
+		if(images[i+1].classList.contains("active") || parseInt(images[i+1].style.left,10) > 30) {
+			images[i].style.left = "15px";
+		}
+		else {
+			images[i].style.left = "40px";
+		}
+		images[i].style.top = offset +"px";
+		
+	}
+	for(i = activebutton+1; i<images.length; i++) {
+		offset = parseInt(images[i-1].style.top,10) +40;
+		if(images[i-1].classList.contains("active") || parseInt(images[i-1].style.left,10) > 30) {
+			images[i].style.left = "15px";
+		}
+		else {
+			images[i].style.left = "40px";
+		}
+		images[i].style.top = offset +"px";
+		
+	}	
+}
+
+
+function checkHeader() {
+	/*if(textfield.scrollTop > 20) {
+		if(!header.classList.contains("scrolling")) {
+			header.classList.add("scrolling");
+			
+		}
+		
+	}
+	else {
+		header.classList.remove("scrolling");
+	}*/
+	
+	if(contentheaders[0].getBoundingClientRect().top-headerHeight > 10) {
+		if(!buttons[0].classList.contains("active")) {
+			
+			for(j=1; j<buttons.length; j++) {
+				buttons[j].classList.remove("active");
+			}
+			buttons[0].classList.add("active");
+		}
+	}
+	else {
+		var lowest = 0;
+		for(var i = 0; i<contentheaders.length; i++) {					
+			if(contentheaders[i].getBoundingClientRect().top-headerHeight < 10) {
+				lowest = i;								
+			}	
+			else {
+				break;
+			}
+		}
+		if(!buttons[lowest+1].classList.contains("active")) {
+			for(j=0; j<buttons.length; j++) {
+				buttons[j].classList.remove("active");
+			}
+			
+			buttons[lowest+1].classList.add("active");							
+		}
+	}
+}
+
+
+
+
+
+
+
+
